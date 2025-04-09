4534fba241cc2242c555360e75c90c41b4e390c7
Pedro Augusto Stabach <pedro.stabach@pucpr.edu.br>
2025-04-08 21:15:00 -0300

    novo menu

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 document.getElementById("toggleMenu").addEventListener("click", function() {
     document.getElementById("menuMob").classList.toggle("ativo");
 });
+
+
+document.getElementById("toggleMenu").addEventListener("click", function() {
+    document.getElementById("mobTrac2").classList.toggle("mobTrac");
+});
+document.getElementById("toggleMenu").addEventListener("click", function() {
+    document.getElementById("mobTrac3").classList.toggle("mobTrac4");
+});
