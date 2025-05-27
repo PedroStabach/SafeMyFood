5f006f2ec50106985eccf76608fb7b15fe3443bf
PedroStabach <pedroaugustostabach3@gmail.com>
2025-05-27 19:29:34 -0300

    headerjs

---
+++ header.js
@@ -0,0 +1,30 @@
+const header = document.getElementById("header").innerHTML = `
+            <div class="headArea">
+            <div id="nameText">SaveMyFood</div>
+        </div>
+        <div class="headeArea">
+            <nav>
+                <ul>
+                    <li><a href="">Inicio</a> </li>
+                    <li><a href="">Familia</a></li>
+                    <li><a href="">Biblioteca</a></li>
+                    <li><a href="">Scanner</a></li>
+                    <li><a href="">Receitas</a></li>
+                    <li><a href="" id="botaoLogin">Entrar</a></li>
+                </ul>
+            </nav>
+        </div>`;
+
+const footer = document.getElementById("footer").innerHTML = `        
+    <div class="footerArea">
+            <nav>
+                <ul>
+                    <li><a href="home.html"><img src="images/home.png" alt=""> </a></li>
+                    <li><a href="family.html"><img src="images/parental-control.png" alt=""></a></li>
+                    <li><a href="biblioteca.html"><img src="images/book.png" alt=""></a></li>
+                    <li><a href="scanner.html"><img src="images/barcode.png" alt=""></a></li>
+                    <li><a href="receitas.html"><img src="images/pencil.png" alt=""></a></li>
+                    <li><a href="cadastro.html"><img src="images/enter.png" alt=""></a></li>
+                </ul>
+            </nav>
+        </div>`;
